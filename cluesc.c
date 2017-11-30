cb2a1a222c726132fe7810b18021050e84e75cbc
Donovan Keohane <admin@donovank.net>
2017-11-30 08:34:36 -0500

    save

--- cluesc.c
+++ cluesc.c
@@ -1,21 +1,28 @@
 //#include <sqlite3.h>
 #include <ncurses.h>
+#include <string.h>
+
+char *splash = "   ___ _            \n"
+                "  / __\\ |_   _  ___ \n"
+                " / /  | | | | |/ _ \\\n"
+                "/ /___| | |_| |  __/\n"
+                "\\____/|_|\\__,_|\\___|\n\n";
 
 int main(int argc, char **argv) {
-	initscr();
-	raw();
-	keypad(stdscr,TRUE);
-	noecho();
+	initscr();            // Initializes the screen
+	keypad(stdscr,TRUE);  // Allows keyboard input
+  noecho();             // Doesn't echo keyboard input
+  cbreak();             // Allows CTRL-C to break
 
-
-	//print
-	printw("Hello world");
+	printw(splash);
 	refresh();
 
-	//get char
-	getch();
+  int c;
+  while (c != '\n') {
+    printw("%c", c);
+    c = getch();
+  }
 
-	//close 
 	endwin();
 	return 0;
 }
