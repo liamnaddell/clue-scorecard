2dcb1a217ab39b0b257bec0905da205e05942d73
Liam Naddell <liamnprg@gmail.com>
2017-12-17 23:05:54 -0500

    segfault

--- main.c
+++ main.c
@@ -11,7 +11,9 @@ struct hand {
 };
 
 int app_close(sqlite3 *db, int rc) {
+	puts("oof");
 	sqlite3_close(db);
+	puts("koof1");
 	return rc;
 }
 
@@ -38,12 +40,11 @@ int app_close(sqlite3 *db, int rc) {
 //	return 0;
 //}
 
-int main() {
-	sqlite3 *db;
+
+int GetHands(sqlite3 *db, struct hand *hand1) {
 	int error = 0;
 	sqlite3_stmt *res;
 	const char *tail;
-	int ret = sqlite3_open("tmpdb_cluesc",&db);
 	char *sql = "select * from hands";
 	error =	sqlite3_prepare(db,sql,strlen(sql),&res,&tail);
 
@@ -51,55 +52,60 @@ int main() {
 		puts("We did not get any data!");
 		return 0;
 	}
-	struct hand hand1[60];
-	int i;
+	int i = 0;
 	while (sqlite3_step(res) == SQLITE_ROW) {
+		puts("hi1");
 		const unsigned char *name = sqlite3_column_text(res,0);	
+		puts("hi2");
 		const unsigned char *card1 = sqlite3_column_text(res,1);	
+		puts("hi3");
 		const unsigned char *card2 = sqlite3_column_text(res,2);	
+		puts("hi4");
 		const unsigned char *card3 = sqlite3_column_text(res,3);	
+		puts("hi5");
 		const unsigned char *card4 = sqlite3_column_text(res,4);	
+		puts("hi6");
 		strcpy(hand1[i].playerName,name);
+		puts("hi7");
 		strcpy(hand1[i].card1,card1);
+		puts("hi8");
 		strcpy(hand1[i].card2,card2);
+		puts("hi9");
 		strcpy(hand1[i].card3,card3);
+		puts("hi10");
 		strcpy(hand1[i].card4,card4);
-		i++;
+		puts("hi11");
+		i=i+1;
+		printf("iperf %d\n",i);
+		if (i >= 5) {
+			puts("BROEKN");
+			break;
+		}
+		puts("LOL");
 
 	}
-	i = i-1;
-	while (i>=0) {
-		printf("%s|%s|%s|%s|%s|\n",hand1[i].playerName,hand1[i].card1,hand1[i].card2,hand1[i].card3,hand1[i].card4);
-		i--;
-	}
+//	printf("ibef %d\n",i);
+	//for (;;) {
+	//	printf("%s|%s|%s|%s|%s|\n",hand1[i].playerName,hand1[i].card1,hand1[i].card2,hand1[i].card3,hand1[i].card4);
+	//	i=i-1;
+	//	printf("ibef %d\n",i);
+	//	if (i <= 0) {
+	//		break;
+	//	}
+	//}
+	puts("kappa");
 	sqlite3_finalize(res);
-    
-
-
-	return app_close(db,0);
+	puts("kappa2");
+	return 0;
 }
 
-
-//int main(int argc, char **argv){
-//	sqlite3 *db;
-//	char *zErrMsg = 0;
-//	int rc;
-//
-//	if( argc!=3 ){
-//		fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-//		return(1);
-//	}
-//	rc = sqlite3_open(argv[1], &db);
-//	if( rc ){
-//		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-//		sqlite3_close(db);
-//		return(1);
-//	}
-//	rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-//	if( rc!=SQLITE_OK ){
-//		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-//		sqlite3_free(zErrMsg);
-//	}
-//	sqlite3_close(db);
-//	return 0;
-//}
+int main() {
+	sqlite3 *db;
+	int ret = sqlite3_open("tmpdb_cluesc",&db);
+	struct hand hand1[4];
+	puts("joof");
+	GetHands(db,hand1);
+	puts("joof3");
+	return app_close(db,0);
+	puts("smkoke waed");
+}
